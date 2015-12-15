Add normalization and fix mirror bugs in predict.py

read.py
@@ -2,24 +2,28 @@
 import numpy as np
 import csv
 import sys
+from sklearn.preprocessing import normalize
 
 def read_sample_train():
     ID_map = []
     ID_check = []
     X_train = []
     Y_train = []
+    readDepthLimit = 100000
     print 'Reading Sample_train_x.csv & Truth_train.csv...'
     with open('../ML_final_project/sample_train_x.csv', 'r') as f:
         reader = csv.reader(f)
-        X_train = np.array(list(reader)[1:]).astype(int)
+        X_train = np.array(list(reader)[1:]).astype(float)
         ID_map = X_train[:, 0]
-        X_train = X_train[:, 1:]
+        #X_train = X_train[:, 1:]
+        # Start from 5
+        X_train = normalize(X_train[:, 5:][:readDepthLimit])
     f.close()
     with open('../ML_final_project/truth_train.csv', 'r') as f:
         reader = csv.reader(f)
         Y_train = np.array(list(reader)).astype(int)
         ID_check = Y_train[:, 0]
-        Y_train = np.ravel(Y_train[:, 1:])
+        Y_train = np.ravel(Y_train[:, 1:])[:readDepthLimit]
     if len(Y_train) != len(X_train):
         print 'Error: Y"s len should equal to X"s len!'
         sys.exit(0)
@@ -35,9 +39,9 @@ def read_truth():
     print 'Reading Truth.csv'
     with open('../ML_final_project/sample_test_x.csv', 'r') as f:
         reader = csv.reader(f)
-        test = np.array(list(reader)[1:]).astype(int)
+        test = np.array(list(reader)[1:]).astype(float)
         ID_map = test[:, 0]
-        test = test[:, 1:]
+        test = normalize(test[:, 5:])
     f.close()
     if len(ID_map) != len(test):
         print 'Error: ID_map len. != test len.'

svm.py
@@ -11,8 +11,8 @@ def main():
    ID_map, X_train, Y_train = read.read_sample_train()
    model = doSVM((X_train, Y_train), (c, k))
    print 'Store the model...'
-   current = int(time.time())
-   joblib.dump(model, 'modle_'+k+'_'+current+'.pkl')
+   current = str(time.time())
+   joblib.dump(model, 'model_'+k+'_'+current+'.pkl')
    print 'Using model to predict'
    predict = model.predict(X_train)
    Ein = np.count_nonzero(predict != Y_train)
@@ -22,7 +22,7 @@ def handleArgv():
     if len(sys.argv) < 3:
         print 'Error: There should be two argv'
         sys.exit(0)
-    if sys.argv[2] != 'rbf' and sys.argv[2] != 'linear':
+    if sys.argv[2] != 'rbf' and sys.argv[2] != 'linear' and sys.argv[2] != 'poly':
         print 'Error: Second argv should be rbf or linear'
         sys.exit(0)
     return float(sys.argv[1]), sys.argv[2]
@@ -32,8 +32,10 @@ def doSVM(data, arg):
     model = 0
     if arg[1] == 'rbf':
         model = svm.SVC(C=arg[0], kernel=arg[1], gamma=1, tol=1e-7, shrinking=True, verbose=True)
+    elif arg[1] == 'linear':
+        model = svm.SVC(C=arg[0], kernel=arg[1], shrinking=True, verbose=True, probability=True)
     else:
-        model = svm.SVC(C=arg[0], kernel=arg[1], shrinking=True, verbose=True)
+        model = svm.SVC(C=arg[0], kernel=arg[1], degree=2, gamma=1, coef0=1, tol=1e-4, shrinking=True, verbose=True)
     model.fit(data[0], data[1])
     return model
 