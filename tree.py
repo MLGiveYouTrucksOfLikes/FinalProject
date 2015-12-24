efac9434b0416aec2a4c39e032024080c0b66f21
GBLin <r04943092@ntu.edu.tw>
2015-12-24 23:52:32 +0800

    Extra Tree so good

--- tree.py
+++ tree.py
@@ -1,5 +1,6 @@
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 import read
 import numpy as np
 from sklearn.externals import joblib
@@ -19,8 +20,8 @@ def tree():
 
     ID_map, X_train, Y_train = read.read_sample_train(normalization_way)
     X_cross_train, X_cross_test, Y_cross_train, Y_cross_test = do_cross_validation(X_train, Y_train, 0.2)
-
-    clf = DecisionTreeClassifier()
+    print 'Making Extreme Random Forest...'
+    clf = ExtraTreesClassifier(n_estimators = 1000, verbose = True)
     clf = clf.fit(X_cross_train, Y_cross_train)
     Y_cross_predict = clf.predict(X_cross_test)
     Eval = np.count_nonzero(Y_cross_predict != Y_cross_test)/float(len(Y_cross_test)) 
